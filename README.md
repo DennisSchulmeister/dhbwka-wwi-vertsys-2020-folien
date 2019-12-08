91fee75215730eeb7a05da66616b42b03f1bb151
Dennis Schulmeister-Zimolong <dennis@patk.org>
2019-12-08 23:26:24 +0100

    Struktur aus der alten Version der Vorlesung übernommen

---
+++ README.md
@@ -0,0 +1,25 @@
+Vorlesung "Verteilte Systeme" im Studiengang Wirtschaftsinformatik an der DHBW Karlsruhe (ab 2020)
+==================================================================================================
+
+Kurzbeschreibung
+----------------
+
+Bei diesen Dateien handelt es sich um das Online-Folienskript zur Vorlesung
+"Verteilte Systeme" im Studiengang Wirtschaftsinformatik an der DHBW Karlsruhe
+ab Studienjahr 2020. Die fertigen Folien befinden sich online auf folgender Seite:
+https://www.wpvs.de/
+
+Die Folien basieren auf [lecture-slides.js](https://github.com/DennisSchulmeister/lecture-slides.js). <br/>
+Klicke hier für eine [Vorlage für eigene Präsentationen](https://github.com/DennisSchulmeister/ls-presentation-template). <br/>
+Und hier für [ausführlichere Demos des Funktionsumfangs](https://github.com/DennisSchulmeister/ls-presentation-demo).
+
+Copyright
+---------
+
+Diese Folien sind lizenziert unter
+[_Creative Commons Namensnennung 4.0 International_](http://creativecommons.org/licenses/by/4.0/)
+
+© 2019 Dennis Schulmeister-Zimolong im Auftrag der DHBW Karlsruhe <br/>
+
+E-Mail: [dhbw@windows3.de](mailto:dhbw@windows3.de) <br/>
+Webseite: https://www.wpvs.de
---
+++ index.js
@@ -0,0 +1,70 @@
+/*
+ * Vorlesung "Verteilte Systeme"
+ * im Studiengang Wirtschaftsinformatik
+ * an der DHBW Karlsurhe
+ *
+ * © 2017 Dennis Schulmeister-Zimolong <dhbw@windows3.de>
+ * Lizenz: Creative Commons Namensnennung 4.0 International
+ *
+ * Sie dürfen:
+ *
+ *     Teilen — das Material in jedwedem Format oder Medium vervielfältigen
+ *     und weiterverbreiten
+ *
+ *     Bearbeiten — das Material remixen, verändern und darauf aufbauen
+ *     und zwar für beliebige Zwecke, sogar kommerziell.
+ *
+ * Unter folgenden Bedingungen:
+ *
+ *     Namensnennung — Sie müssen angemessene Urheber- und Rechteangaben
+ *     machen, einen Link zur Lizenz beifügen und angeben, ob Änderungen
+ *     vorgenommen wurden. Diese Angaben dürfen in jeder angemessenen Art
+ *     und Weise gemacht werden, allerdings nicht so, dass der Eindruck
+ *     entsteht, der Lizenzgeber unterstütze gerade Sie oder Ihre Nutzung
+ *     besonders.
+ *
+ *     Keine weiteren Einschränkungen — Sie dürfen keine zusätzlichen Klauseln
+ *     oder technische Verfahren einsetzen, die anderen rechtlich irgendetwas
+ *     untersagen, was die Lizenz erlaubt.
+ *
+ * Es werden keine Garantien gegeben und auch keine Gewähr geleistet.
+ * Die Lizenz verschafft Ihnen möglicherweise nicht alle Erlaubnisse,
+ * die Sie für die jeweilige Nutzung brauchen. Es können beispielsweise
+ * andere Rechte wie Persönlichkeits- und Datenschutzrechte zu beachten
+ * sein, die Ihre Nutzung des Materials entsprechend beschränken.
+ */
+"use strict";
+
+import emailLinkJs from "email-link.js";
+
+import SlideshowPlayer from "lecture-slides.js";
+import "./style/style.less";
+
+import LsPluginHighlightJs from "ls-plugin-highlight.js";
+import "highlight.js/styles/atom-one-light.css";
+
+window.addEventListener("load", () => {
+    emailLinkJs.enableEmailLinks();
+
+    let player = new SlideshowPlayer({
+        labelPrev: "Zurück",
+        labelNext: "Weiter",
+        labelGoTo: "Gehe zu",
+        labelViewMenu: "Ansicht",
+        labelOverview: "Übersicht",
+        labelSlidesAndText: "Folien und Text",
+        labelSlidesOnly: "Nur Folien",
+        labelTextOnly: "Nur Text",
+        labelPrintView: "Drucken",
+        labelFadeToWhite: "Alles ausblenden (weiß)",
+        labelFadeToBlack: "Alles ausblenden (schwarz)",
+        labelFadeBack: "Klicken, um zu den Folien zurückzukehren",
+        mode: "overview",
+        linkMode: "slideshow",
+        plugins: {
+            HighlightJs: new LsPluginHighlightJs(),
+        }
+    });
+
+    player.start();
+});
